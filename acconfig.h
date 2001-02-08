a8565ab569e7c120cf558d0631e3f648febad285
Alain Magloire <alainm@rcsm.ee.mcgill.ca>
2001-02-08 21:55:02 +0000

    doc fixes.
    * doc/grep.texi : Typo fixes.

--- acconfig.h
+++ acconfig.h
@@ -13,6 +13,18 @@
 /* Hack for Visual C++ suggested by irox. */
 #undef alloca
 
+/* Internationalization stuffs.  */
+
+#undef HAVE_STPCPY
+
+#undef ENABLE_NLS
+
+#undef HAVE_CATGETS
+
+#undef HAVE_GETTEXT
+
+#undef HAVE_LC_MESSAGES
+
 /* Define if <stdlib.h> declares strtoul.  */
 #undef HAVE_DECL_STRTOUL
 
--- po/cat-id-tbl.c
+++ po/cat-id-tbl.c
@@ -28,88 +28,88 @@ const struct _msg_ent _msg_tbl[] = {
   {"%s: option requires an argument -- %c\n", 19},
   {"%s: option `-W %s' is ambiguous\n", 20},
   {"%s: option `-W %s' doesn't allow an argument\n", 21},
-  {"option %s", 22},
-  {" with arg %s", 23},
-  {"digits occur in two different argv-elements.\n", 24},
-  {"option %c\n", 25},
-  {"option a\n", 26},
-  {"option b\n", 27},
-  {"option c with value `%s'\n", 28},
-  {"option d with value `%s'\n", 29},
-  {"?? getopt returned character code 0%o ??\n", 30},
-  {"non-option ARGV-elements: ", 31},
-  {"memory exhausted", 32},
-  {"%s: warning: %s: %s\n", 33},
-  {"writing output", 34},
-  {"Binary file %s matches\n", 35},
-  {"(standard input)", 36},
-  {"Usage: %s [OPTION]... PATTERN [FILE]...\n", 37},
-  {"Try `%s --help' for more information.\n", 38},
-  {"Usage: %s [OPTION]... PATTERN [FILE] ...\n", 39},
+  {"memory exhausted", 22},
+  {"invalid context length argument", 23},
+  {"input is too large to count", 24},
+  {"writing output", 25},
+  {"Binary file %s matches\n", 26},
+  {"(standard input)", 27},
+  {"%s: warning: %s: %s\n", 28},
+  {"recursive directory loop", 29},
+  {"Usage: %s [OPTION]... PATTERN [FILE]...\n", 30},
+  {"Try `%s --help' for more information.\n", 31},
+  {"Usage: %s [OPTION]... PATTERN [FILE] ...\n", 32},
   {"\
 Search for PATTERN in each FILE or standard input.\n\
+Example: %s -i 'hello world' menu.h main.c\n\
 \n\
-Regexp selection and interpretation:\n\
+Regexp selection and interpretation:\n", 33},
+  {"\
   -E, --extended-regexp     PATTERN is an extended regular expression\n\
-  -F, --fixed-regexp        PATTERN is a fixed string separated by newlines\n\
+  -F, --fixed-strings       PATTERN is a set of newline-separated strings\n\
   -G, --basic-regexp        PATTERN is a basic regular expression\n\
+  -P, --perl-regexp         PATTERN is a Perl regular expression\n", 34},
+  {"\
   -e, --regexp=PATTERN      use PATTERN as a regular expression\n\
   -f, --file=FILE           obtain PATTERN from FILE\n\
   -i, --ignore-case         ignore case distinctions\n\
   -w, --word-regexp         force PATTERN to match only whole words\n\
-  -x, --line-regexp         force PATTERN to match only whole lines\n", 40},
+  -x, --line-regexp         force PATTERN to match only whole lines\n\
+  -z, --null-data           a data line ends in 0 byte, not newline\n", 35},
   {"\
 \n\
 Miscellaneous:\n\
   -s, --no-messages         suppress error messages\n\
-  -v, --revert-match        select non-matching lines\n\
+  -v, --invert-match        select non-matching lines\n\
   -V, --version             print version information and exit\n\
-      --help                display this help and exit\n", 41},
+      --help                display this help and exit\n\
+      --mmap                use memory-mapped input if possible\n", 36},
   {"\
 \n\
 Output control:\n\
+  -m, --max-count=NUM       stop after NUM matches\n\
   -b, --byte-offset         print the byte offset with output lines\n\
   -n, --line-number         print line number with output lines\n\
   -H, --with-filename       print the filename for each match\n\
   -h, --no-filename         suppress the prefixing filename on output\n\
   -q, --quiet, --silent     suppress all normal output\n\
-  -a, --text                do not suppress binary output\n\
+      --binary-files=TYPE   assume that binary files are TYPE\n\
+                            TYPE is 'binary', 'text', or 'without-match'.\n\
+  -a, --text                equivalent to --binary-files=text\n\
+  -I                        equivalent to --binary-files=without-match\n\
   -d, --directories=ACTION  how to handle directories\n\
                             ACTION is 'read', 'recurse', or 'skip'.\n\
   -r, --recursive           equivalent to --directories=recurse.\n\
   -L, --files-without-match only print FILE names containing no match\n\
   -l, --files-with-matches  only print FILE names containing matches\n\
-  -c, --count               only print a count of matching lines per FILE\n", 42},
+  -c, --count               only print a count of matching lines per FILE\n\
+  -Z, --null                print 0 byte after FILE name\n", 37},
   {"\
 \n\
 Context control:\n\
   -B, --before-context=NUM  print NUM lines of leading context\n\
   -A, --after-context=NUM   print NUM lines of trailing context\n\
-  -C, --context[=NUM]       print NUM (default 2) lines of output context\n\
-                            unless overriden by -A or -B\n\
-  -NUM                      same as --contex=NUM\n\
+  -C, --context=NUM         print NUM lines of output context\n\
+  -NUM                      same as --context=NUM\n\
   -U, --binary              do not strip CR characters at EOL (MSDOS)\n\
   -u, --unix-byte-offsets   report offsets as if CRs were not there (MSDOS)\n\
 \n\
-If no -[GEF], then `egrep' assumes -E, `fgrep' -F, else -G.\n\
-With no FILE, or when FILE is -, read standard input. If less than\n\
-two FILEs given, assume -h. Exit with 0 if matches, with 1 if none.\n\
-Exit with 2 if syntax errors or system errors.\n", 43},
+`egrep' means `grep -E'.  `fgrep' means `grep -F'.\n\
+With no FILE, or when FILE is -, read standard input.  If less than\n\
+two FILEs given, assume -h.  Exit status is 0 if match, 1 if no match,\n\
+and 2 if trouble.\n", 38},
   {"\
 \n\
-Report bugs to <bug-gnu-utils@gnu.org>.\n", 44},
-  {"invalid context length argument", 45},
-  {"infalid context length argument", 46},
-  {"you may specify only one of -E, -F, or -G", 47},
-  {"matcher already specified", 48},
-  {"unknown directories method", 49},
-  {"grep (GNU grep) %s\n", 50},
-  {"\
-Copyright (C) 1988, 92, 93, 94, 95, 96, 97 Free Software Foundation, Inc.\n", 51},
+Report bugs to <bug-gnu-utils@gnu.org>.\n", 39},
+  {"conflicting matchers specified", 40},
+  {"unknown directories method", 41},
+  {"invalid max count", 42},
+  {"unknown binary-files type", 43},
+  {"%s (GNU grep) %s\n", 44},
+  {"Copyright 1988, 1992-1999, 2000 Free Software Foundation, Inc.\n", 45},
   {"\
 This is free software; see the source for copying conditions. There is NO\n\
-warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n", 52},
-  {"memory exhausted\n", 53},
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n", 46},
 };
 
-int _msg_tbl_length = 53;
+int _msg_tbl_length = 46;
