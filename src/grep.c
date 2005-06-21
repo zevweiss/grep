6efca6e7e7e973ac8941c8694cb956efc9764164
Charles Levert <charles_levert@gna.org>
2005-06-21 04:55:26 +0000

    * src/grep.c (nlscan): Make this function more robust by removing   the undocumented assumption that its "lim" argument points   right after a line boundary.  This will be used later to fix   --byte-offset's broken behavior.  Patch #3769.

--- src/grep.c
+++ src/grep.c
@@ -589,8 +589,13 @@ nlscan (char const *lim)
 {
   size_t newlines = 0;
   char const *beg;
-  for (beg = lastnl; beg != lim; beg = memchr (beg, eolbyte, lim - beg), beg++)
-    newlines++;
+  for (beg = lastnl; beg < lim; beg++)
+    {
+      beg = memchr (beg, eolbyte, lim - beg);
+      if (!beg)
+	break;
+      newlines++;
+    }
   totalnl = add_count (totalnl, newlines);
   lastnl = lim;
 }
