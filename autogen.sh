d02c42bc884d961bccbc0aab721c2225ca8e4094
Bernhard Rosenkraenzer <bero@arklinux.org>
2006-08-18 21:16:07 +0000

    Autogenerate ALL_LINGUAS so we don't have to update it manually every time

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,17 @@
 
 chmod +x tests/*.sh   # fix a bug in CVS
 
+# Generate configure.ac with ALL_LINGUAS matching the languages
+# we have...
+ALL_LINGUAS=""
+for i in po/*.po; do
+	ALL_LINGUAS="$ALL_LINGUAS `basename $i .po`"
+done
+# Eliminate leading whitespace
+ALL_LINGUAS="`echo $ALL_LINGUAS |cut -b1-`"
+sed -e "s,@ALL_LINGUAS@,$ALL_LINGUAS," configure.ac.in >configure.ac
+
+# Let auto* do its work
 if \
 aclocal -I m4 &&
 autoheader &&
