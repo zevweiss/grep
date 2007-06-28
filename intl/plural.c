e7ac713d0dd052f897417be9779d761dbae4e609
Bernhard Rosenkraenzer <bero@arklinux.org>
2007-06-28 19:10:42 +0000

    GPLv2 -> GPLv3

--- intl/plural.c
+++ intl/plural.c
@@ -25,7 +25,7 @@
 
    This program is free software; you can redistribute it and/or modify it
    under the terms of the GNU Library General Public License as published
-   by the Free Software Foundation; either version 2, or (at your option)
+   by the Free Software Foundation; either version 3, or (at your option)
    any later version.
 
    This program is distributed in the hope that it will be useful,
@@ -307,7 +307,7 @@ static const short yycheck[] = {     1,
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
+   the Free Software Foundation; either version 3, or (at your option)
    any later version.
 
    This program is distributed in the hope that it will be useful,
