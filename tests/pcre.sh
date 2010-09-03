bbc7f623e6fd4f0f0f5f19f9c41e544c4e993371
Jim Meyering <meyering@redhat.com>
2010-09-03 10:17:01 +0200

    tests: convert pcre.sh to use init.sh
    * tests/pcre.sh: Use init.sh. Use Exit, not exit. Use grep, not ${GREP}.

--- tests/pcre.sh
+++ tests/pcre.sh
@@ -7,16 +7,17 @@
 # are permitted in any medium without royalty provided the copyright
 # notice and this notice are preserved.
 
-: ${GREP=../src/grep}
+. "${srcdir=.}/init.sh"; path_prepend_ ../src
 
 # Test that grep was compiled with HAVE_LIBPCRE.  Otherwise, pass.
-echo . | ${GREP} -P . >/dev/null 2>&1 || exit 0
+echo . | grep -P . >/dev/null 2>&1 || exit 0
 
-fs=0
+fail=0
 ft=
 
 # See CVS revision 1.32 of "src/search.c".
-echo | { ${GREP} -P '\s*$'; } > /dev/null 2>&1 || { ft="$ft 1"; fs=1; }
+echo | { grep -P '\s*$'; } > /dev/null 2>&1 || { ft="$ft 1"; fail=1; }
 
 test "x$ft" != x && echo "Failed PCRE tests:$ft"
-exit $fs
+
+Exit $fail
