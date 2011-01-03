daaf7ce73d24798fee710ce6e8948505c9905964
Jim Meyering <meyering@redhat.com>
2011-01-03 22:00:08 +0100

    maint: update copyright year ranges to include 2011
    Run "make update-copyright", so "make syntax-check" works in 2011.

--- lib/savedir.c
+++ lib/savedir.c
@@ -1,5 +1,5 @@
 /* savedir.c -- save the list of files in a directory in a string
-   Copyright (C) 1990, 1997-2001, 2009-2010 Free Software Foundation, Inc.
+   Copyright (C) 1990, 1997-2001, 2009-2011 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
