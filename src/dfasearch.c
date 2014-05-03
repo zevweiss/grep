61c8431a8cb6b6de247e833e1daf69df1d454c96
Paul Eggert <eggert@cs.ucla.edu>
2014-05-03 15:37:17 -0700

    grep: clarify EGexecute slightly
    * src/dfasearch.c (EGexecute): Change if-then-else to !if-else-then.

--- src/dfasearch.c
+++ src/dfasearch.c
@@ -257,9 +257,9 @@ EGexecute (char const *buf, size_t size, size_t *match_size,
               end = ((exact_kwset_match || !dfafast
                       || MAX (16, match - beg) < (match - prev_beg) >> 2)
                      ? match
-                     : (buflim - prev_beg) >> 2 <= MAX (16, match - beg)
-                     ? buflim
-                     : prev_beg + 4 * MAX (16, match - beg));
+                     : MAX (16, match - beg) < (buflim - prev_beg) >> 2
+                     ? prev_beg + 4 * MAX (16, match - beg)
+                     : buflim);
               end = memchr (end, eol, buflim - end);
               end = end ? end + 1 : buflim;
 
