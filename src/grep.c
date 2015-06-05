6cb9d18d7fa8b5ea04451e92c22cfe2ef07e7197
Zev Weiss <zev@bewilderbeest.net>
2015-06-04 22:17:23 -0500

    Cleanup: remove unnecessary dirdesc variable.
    We can just use fts_cwd_fd directly, since the fts_flags test was
    guaranteed to succeed (and it was already being used directly in
    fstatat() anyway).

--- src/grep.c
+++ src/grep.c
@@ -1496,7 +1496,6 @@ static bool
 grepdirent (struct grepctx *ctx, FTS *fts, FTSENT *ent, bool command_line)
 {
   bool follow;
-  int dirdesc;
   command_line &= ent->fts_level == FTS_ROOTLEVEL;
 
   if (ent->fts_info == FTS_DP)
@@ -1572,10 +1571,8 @@ grepdirent (struct grepctx *ctx, FTS *fts, FTSENT *ent, bool command_line)
       abort ();
     }
 
-  dirdesc = ((fts->fts_options & (FTS_NOCHDIR | FTS_CWDFD)) == FTS_CWDFD
-             ? fts->fts_cwd_fd
-             : AT_FDCWD);
-  return grepfile (ctx, dirdesc, ent->fts_accpath, follow, command_line);
+  return grepfile (ctx, fts->fts_cwd_fd, ent->fts_accpath, follow,
+                   command_line);
 }
 
 /* True if errno is ERR after 'open ("symlink", ... O_NOFOLLOW ...)'.
