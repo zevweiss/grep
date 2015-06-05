92428244ba9aa7418f8ff0710f133876c1a19f68
Zev Weiss <zev@bewilderbeest.net>
2015-06-05 16:05:42 -0500

    Cleanup: move regex syntax configuration into struct dfa.

--- src/dfa.h
+++ src/dfa.h
@@ -50,10 +50,10 @@ extern struct dfamust *dfamust (struct dfa const *);
 /* Free the storage held by the components of a struct dfamust. */
 extern void dfamustfree (struct dfamust *);
 
-/* dfasyntax() takes three arguments; the first sets the syntax bits described
-   earlier in this file, the second sets the case-folding flag, and the
-   third specifies the line terminator. */
-extern void dfasyntax (reg_syntax_t, int, unsigned char);
+/* dfasyntax() takes four arguments; the first is the dfa to operate one, the
+   second sets the syntax bits described earlier in this file, the third sets
+   the case-folding flag, and the fourth specifies the line terminator. */
+extern void dfasyntax (struct dfa *, reg_syntax_t, int, unsigned char);
 
 /* Compile the given string of the given length into the given struct dfa.
    Final argument is a flag specifying whether to build a searching or an
@@ -88,9 +88,6 @@ extern bool dfaisfast (struct dfa const *) _GL_ATTRIBUTE_PURE;
 extern void dfafree (struct dfa *);
 
 /* Entry points for people who know what they're doing. */
-
-/* Initialize the components of a struct dfa. */
-extern void dfainit (struct dfa *);
 
 /* Incrementally parse a string of given length into a struct dfa. */
 extern void dfaparse (char const *, size_t, struct dfa *);
