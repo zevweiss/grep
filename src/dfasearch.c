dfc42a1a58a1802925d411b59e3c9aa78b06e21a
Zev Weiss <zev@bewilderbeest.net>
2015-06-06 22:21:37 -0500

    dfasearch: eliminate a static local variable.
    dfawarn() shouldn't be a hot path, so having a getenv() call in it
    should be fine.

--- src/dfasearch.c
+++ src/dfasearch.c
@@ -75,10 +75,7 @@ dfaerror (char const *mesg)
 void
 dfawarn (char const *mesg)
 {
-  static enum { DW_NONE = 0, DW_POSIX, DW_GNU } mode;
-  if (mode == DW_NONE)
-    mode = (getenv ("POSIXLY_CORRECT") ? DW_POSIX : DW_GNU);
-  if (mode == DW_GNU)
+  if (!getenv ("POSIXLY_CORRECT"))
     dfaerror (mesg);
 }
 
