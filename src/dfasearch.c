53bcb850a07327a06f265705b80b0b16bb558a73
Zev Weiss <zev@bewilderbeest.net>
2015-06-06 22:16:44 -0500

    dfa: eliminate static local variables.

--- src/dfasearch.c
+++ src/dfasearch.c
@@ -277,7 +277,7 @@ EGexecute (void *vdc, struct grepctx *ctx, char const *buf, size_t size,
 
               if (exact_kwset_match)
                 {
-                  if (MB_CUR_MAX == 1 || using_utf8 ())
+                  if (MB_CUR_MAX == 1 || using_utf8)
                     goto success;
                   if (mb_start < beg)
                     mb_start = beg;
