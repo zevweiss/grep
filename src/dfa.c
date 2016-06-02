3ddf995f9c9651d8ca8a6d23655e956cb807fb36
Norihiro Tanaka <noritnk@kcn.ne.jp>
2016-06-02 15:48:56 -0700

    dfa: more simplification of dfaexec_main
    * src/dfa.c (dfaexec_main): Failure at an acceptable position and demand
    to build state is unlikely.  So go next loop without checking them after
    a newline.  This commit induces no semantic change.

--- src/dfa.c
+++ src/dfa.c
@@ -3372,8 +3372,7 @@ dfaexec_main (struct dfa *d, char const *begin, char *end, bool allow_nl,
 
           s = allow_nl ? d->newlines[s1] : 0;
         }
-
-      if (d->fails[s])
+      else if (d->fails[s])
         {
           if (d->success[s] & sbit[*p])
             goto done;
@@ -3386,8 +3385,7 @@ dfaexec_main (struct dfa *d, char const *begin, char *end, bool allow_nl,
         }
       else
         {
-          if (!d->trans[s])
-            build_state (s, d);
+          build_state (s, d);
           trans = d->trans;
         }
     }
