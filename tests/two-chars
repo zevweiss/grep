zevweiss/grep

commit 966f6586fbce3081ce6e5e2f9b55301b0ec3d2b4

grep: use memchr2 for two patterns of a character
* src/kwset.c (memchr2_kwset): Add a new function.  grep uses memchr2 to
search just two letters.
(cwexec, acexec_trans): Use it.

src/kwset.c
@@ -640,6 +640,31 @@ memchr_kwset (char const *s, size_t n, kwset_t kwset)
   return n == 0 ? NULL : memchr2 (s, kwset->gc1, kwset->gc1help, n);
 }
 
+/* Return the address of the first byte in the buffer S (of size N)
+   that matches the last byte specified by KWSET, a singleton.  */
+static size_t
+memchr2_kwset (char const *s, size_t n, kwset_t kwset,
+               struct kwsmatch *kwsmatch)
+{
+  struct tree const *link = kwset->trie->links;
+  struct tree const *clink = link->llink ? link->llink : link->rlink;
+
+  char const *mch = memchr2 (s, link->label, clink->label, n);
+  if (mch)
+    {
+      size_t off = mch - s;
+      if (*mch == link->label)
+        kwsmatch->index = link->trie->accepting / 2;
+      else
+        kwsmatch->index = clink->trie->accepting / 2;
+      kwsmatch->offset[0] = off;
+      kwsmatch->size[0] = 1;
+      return off;
+    }
+  else
+    return -1;
+}
+
 /* Fast Boyer-Moore search (inlinable version).  */
 static inline size_t _GL_ATTRIBUTE_PURE
 bmexec_trans (kwset_t kwset, char const *text, size_t size)
@@ -768,6 +793,8 @@ cwexec (kwset_t kwset, char const *text, size_t len,
   /* Initialize register copies and look for easy ways out. */
   if (len < kwset->mind)
     return -1;
+  if (!kwset->trans && kwset->maxd == 1 && kwset->words == 2)
+    return memchr2_kwset (text, len, kwset, kwsmatch);
   next = kwset->next;
   delta = kwset->delta;
   trans = kwset->trans;
@@ -925,6 +952,8 @@ acexec_trans (kwset_t kwset, char const *text, size_t len,
   /* Initialize register copies and look for easy ways out. */
   if (len < kwset->mind)
     return -1;
+  if (!kwset->trans && kwset->maxd == 1 && kwset->words == 2)
+    return memchr2_kwset (text, len, kwset, kwsmatch);
 
   next = kwset->next;
   trans = kwset->trans;

commit cf39d76da476b747b1b875bdd92dd00770ae4936

grep: fix -F crash when alternating duplicates
grep -F crashes with a pattern like 0\n0.
This bug was introduced in 966f6586fbce3081ce6e5e2f9b55301b0ec3d2b4.

* src/kwset.c (memoff2_kwset): If two characters are the same,
use memchr instead of memchr2.
* tests/two-chars: New test.
* tests/Makefile.am (TESTS): Add it.

src/kwset.c
@@ -643,8 +643,13 @@ memoff2_kwset (char const *s, size_t n, kwset_t kwset,
 {
   struct tree const *link = kwset->trie->links;
   struct tree const *clink = link->llink ? link->llink : link->rlink;
+  char const *mch;
 
-  char const *mch = memchr2 (s, link->label, clink->label, n);
+  if (clink)
+    mch = memchr2 (s, link->label, clink->label, n);
+  else
+    mch = memchr (s, link->label, n);
+
   if (! mch)
     return SIZE_MAX;
   else

tests/Makefile.am
@@ -134,6 +134,7 @@ TESTS =						\
   turkish-I					\
   turkish-I-without-dot				\
   turkish-eyes					\
+  two-chars					\
   two-files					\
   unibyte-binary				\
   unibyte-bracket-expr				\

tests/two-chars
@@ -1,0 +1,24 @@
+#! /bin/sh
+# Check for grep -F with two patterns consisting of the same char.
+#
+# Copyright 2016 Free Software Foundation, Inc.
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.
+
+. "${srcdir=.}/init.sh"; path_prepend_ ../src
+
+require_en_utf8_locale_
+
+fail=0
+
+for LOC in en_US.UTF-8 $zh $LOCALE_FR_UTF8; do
+  printf '0\n0\n' >pat
+  printf '0\n' >in
+  out=out-$LOC
+  LC_ALL=$LOC grep -Ff pat in >$out || fail=1
+  compare in $out || fail=1
+done
+
+Exit $fail