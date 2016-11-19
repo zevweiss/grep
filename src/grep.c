a99b612d1a2ce392a7ca6c59724013a965e81e04
Paul Eggert <eggert@cs.ucla.edu>
2016-11-19 02:49:15 -0800

    grep: -T no longer outputs BS
    * NEWS: Document this (Bug#24451).
    * src/grep.c (print_line_head): Do not attempt to backspace output.
    * tests/initial-tab: New test.
    * tests/Makefile.am (TESTS): Add it.

--- src/grep.c
+++ src/grep.c
@@ -1134,13 +1134,11 @@ print_line_head (char *beg, size_t len, char const *lim, char sep)
         }
     }
 
-  bool pending_sep = false;
-
   if (out_file)
     {
       print_filename ();
       if (filename_mask)
-        pending_sep = true;
+        print_sep (sep);
       else
         putchar_errno (0);
     }
@@ -1153,33 +1151,20 @@ print_line_head (char *beg, size_t len, char const *lim, char sep)
           totalnl = add_count (totalnl, 1);
           lastnl = lim;
         }
-      if (pending_sep)
-        print_sep (sep);
       print_offset (totalnl, 4, line_num_color);
-      pending_sep = true;
+      print_sep (sep);
     }
 
   if (out_byte)
     {
       uintmax_t pos = add_count (totalcc, beg - bufbeg);
       pos = dossified_pos (pos);
-      if (pending_sep)
-        print_sep (sep);
       print_offset (pos, 6, byte_num_color);
-      pending_sep = true;
-    }
-
-  if (pending_sep)
-    {
-      /* This assumes sep is one column wide.
-         Try doing this any other way with Unicode
-         (and its combining and wide characters)
-         filenames and you're wasting your efforts.  */
-      if (align_tabs)
-        fputs_errno ("\t\b");
-
       print_sep (sep);
     }
+
+  if (align_tabs && (out_file | out_line | out_byte) && len != 0)
+    putchar_errno ('\t');
 
   return true;
 }
